bf1302a95ea462525334770f4965bd6850d4f8b1
ZheyuanSun <149382404+ZheyuanSun@users.noreply.github.com>
2023-10-30 22:50:59 +1100

    Create toolkit_config.py

---
+++ lectures/toolkit_config.py
@@ -0,0 +1,11 @@
+""" toolkit_config.py
+
+Project configuration file
+"""
+import os
+
+PRJDIR = r'C:\Users\Zheyuan\PycharmProjects\toolkit'
+
+ROOTDIR = os.path.join(cfg.PRJDIR,'project1')
+DATDIR = os.path.join(ROOTDIR,"data")
+TICPATH = os.path.join(ROOTDIR,"TICKERS.txt")
